14c9f624c60aca60cc12854efd960cf7a6db6420
SpecterV5Fuctions <specterxoficial@gmail.com>
2025-07-13 14:27:42 -0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -12,7 +12,7 @@ return {
 
     --// WHITELIST OWNER (poder total)
     whitelistOwner = {
-        [333333333] = true, -- owner
+        [8913029108] = true, -- owner
     },
 
     --// BLACKLIST (jogadores que são expulsos automaticamente)
