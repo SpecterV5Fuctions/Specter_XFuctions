0517a2addc8feeebc01a71ea566c8290c2d0bee4
SpecterV5Fuctions <specterxoficial@gmail.com>
2025-07-13 14:16:44 -0300

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,22 @@
+return {
+    --// WHITELIST GERAL (usuários que podem usar o script normalmente)
+    whitelist = {
+        [111111111] = true, -- member
+        [222222222] = true, -- member
+    },
+
+    --// WHITELIST ADMIN (pode usar comandos como kick, mas não pode kickar o owner)
+    whitelistAdmin = {
+        [333333333] = true, -- admin
+    },
+
+    --// WHITELIST OWNER (poder total)
+    whitelistOwner = {
+        [8913029108] = true, -- owner
+    },
+
+    --// BLACKLIST (jogadores que são expulsos automaticamente)
+    blacklist = {
+        [444444444] = true, -- banido
+    }
+}
